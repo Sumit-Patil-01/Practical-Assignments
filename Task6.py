9827d5574a528397d3efda4b8bc0f2b7a83245fc
Sumit-Patil-01 <sumitpatil.2b11@gmail.com>
2026-03-31 22:38:46 +0530

    Add star pattern printing in Task6.py

---
+++ Task6.py
@@ -0,0 +1,10 @@
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+# Lower part (4 to 1 stars)
+for i in range(4, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
