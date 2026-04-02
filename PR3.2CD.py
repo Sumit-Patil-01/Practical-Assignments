00f98aa95d00c185d8baebb7e876dc35220957b8
Sumit-Patil-01 <sumitpatil.2b11@gmail.com>
2026-04-02 08:29:16 +0530

    Add script to calculate the sum of numbers up to n

---
+++ PR3.2CD.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a positive integer: "))
+
+if (n < 1):
+    print("Please enter a number greater than 0")
+else:
+    s = 0
+    for i in range(1, n + 1):
+        s = s + i
+    print("The sum of numbers from 1 to", n, "is:", s)
